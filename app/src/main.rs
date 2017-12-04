622b9bd174d933338ee6a1e4b65119b6660d9241
MarcManiez <marc.maniez@gmail.com>
2017-12-03 19:08:33 -0800

    Ascertain that users can be read from database.

--- app/src/main.rs
+++ app/src/main.rs
@@ -7,11 +7,23 @@ extern crate iron;
 pub mod models;
 pub mod schema;
 mod connection;
+
+use std::ops::Deref;
+
 use ::models::user::User;
+use ::schema::users::dsl::*;
+use diesel::prelude::*;
 use iron::prelude::*;
 use iron::status;
 
 fn main() {
+
+    let pool = connection::establish_connection_pool();
+    let connection = pool.get().expect("Failed to fetch a connection.");
+    let all_users = users.load::<User>(connection.deref()).expect("Error loading users.");
+
+    println!("the user: {:?}", all_users[0]);
+
     fn hello_world(_: &mut Request) -> IronResult<Response> {
         Ok(Response::with((status::Ok, "Hello World!")))
     }
