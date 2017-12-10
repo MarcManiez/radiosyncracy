0aeadb8c1371f8349628798209e02651f6121da5
MarcManiez <marc.maniez@gmail.com>
2017-12-09 18:15:54 -0800

    Add route to create users.

--- app/src/api_routes.rs
+++ app/src/api_routes.rs
@@ -6,5 +6,6 @@ use self::router::Router;
 pub fn get_routes() -> Router {
     let mut router = Router::new();
     router.get("/users", users_controller::get_all_users, "resource");
+    router.post("/users", users_controller::create, "resource");
     router
 }
