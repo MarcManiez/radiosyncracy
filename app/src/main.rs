65666061884a69c0e505af4a65a01a503b024a88
MarcManiez <marc.maniez@gmail.com>
2017-12-09 16:46:41 -0800

    Clean up zombie code in main file.

--- app/src/main.rs
+++ app/src/main.rs
@@ -16,12 +16,6 @@ mod controllers;
 use iron::prelude::*;
 
 fn main() {
-    // let pool = connection::establish_connection_pool();
-    // let connection = pool.get().expect("Failed to fetch a connection.");
-    // let all_users = users.load::<User>(connection.deref()).expect("Error loading users.");
-
-    // println!("the user: {:?}", all_users[0]);
-
     let routes = api_routes::get_routes();
 
     let _server = Iron::new(routes).http("localhost:3000").unwrap();
