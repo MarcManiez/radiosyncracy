819227723a2e48c28c79f003e01b482b28075fbc
MarcManiez <marc.maniez@gmail.com>
2017-12-09 18:18:11 -0800

    Add insertable user class.

--- app/src/models/user.rs
+++ app/src/models/user.rs
@@ -10,3 +10,20 @@ pub struct User {
     pub updated_at: Option<NaiveDateTime>,
     pub password: String,
 }
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub email: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: Option<NaiveDateTime>,
+    pub password: String,
+}
+
+#[derive(Debug, Insertable)]
+#[table_name="users"]
+pub struct NewUser<'a> {
+    pub username: &'a str,
+    pub email: &'a str,
+    pub password: &'a str,
+}
+
