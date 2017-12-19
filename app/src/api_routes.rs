0f768349558f9e35f155ea3b7163dcb9c5673136
MarcManiez <marc.maniez@gmail.com>
2017-12-18 21:24:10 -0800

    Create route for sessions.

--- app/src/api_routes.rs
+++ app/src/api_routes.rs
@@ -5,7 +5,8 @@ use self::router::Router;
 
 pub fn get_routes() -> Router {
     let mut router = Router::new();
-    router.get("/users", users_controller::get_all_users, "resource");
-    router.post("/users", users_controller::create, "resource");
+    router.post("/sessions", sessions_controller::create, "sessions#create");
+    router.get("/users", users_controller::get_all_users, "users#get_all_users");
+    router.post("/users", users_controller::create, "users#create");
     router
 }
