f009028994f257048257ec3e3d232531b05b19cd
MarcManiez <marc.maniez@gmail.com>
2017-12-19 21:14:55 -0800

    Remove dead code from users controller.

--- app/src/controllers/users_controller.rs
+++ app/src/controllers/users_controller.rs
@@ -1,32 +1,10 @@
-use diesel::prelude::*;
 use iron::prelude::*;
 use iron::status;
-use serde_json;
 use urlencoded::UrlEncodedBody;
-use urlencoded::UrlEncodedQuery;
-
-use std::ops::Deref;
 
 use ::models::user::User;
-use ::schema::users::dsl::*;
 use super::utils::*;
-use ::connection::POOL;
 use ::views::api::users;
-
-// first test method we implemented to test reading from the database
-pub fn get_all_users(req: &mut Request) -> IronResult<Response> {
-    let query_params = req.get_ref::<UrlEncodedQuery>().expect("Failed to fetch query params.");
-    let email_address = &query_params["email"][0];
-
-    let database_connection = POOL.get().expect("Failed to fetch a connection.");
-
-    let all_users = users.filter(email.eq(email_address))
-        .load::<User>(database_connection.deref())
-        .expect("Error loading users.");
-
-    let serialized_users = serde_json::to_string(&all_users).expect("Failed to serialize users");
-    Ok(Response::with((status::Ok, serialized_users)))
-}
 
 pub fn create(req: &mut Request) -> IronResult<Response> {
     let request_body = req.get_ref::<UrlEncodedBody>().expect("Failed to fetch query params.");
