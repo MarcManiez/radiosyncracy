67ed8c1cd7aad45396c64937abdf337a7bae1b7b
MarcManiez <marc.maniez@gmail.com>
2017-12-19 22:59:30 -0800

    Add route for creating tracks.

--- app/src/api_routes.rs
+++ app/src/api_routes.rs
@@ -6,6 +6,7 @@ use self::router::Router;
 pub fn get_routes() -> Router {
     let mut router = Router::new();
     router.post("/sessions", sessions_controller::create, "sessions#create");
+    router.post("/tracks", tracks_controller::create, "tracks#create");
     router.post("/users", users_controller::create, "users#create");
     router
 }
