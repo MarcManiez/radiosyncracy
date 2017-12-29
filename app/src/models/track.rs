947027e5a69fe5f5bba897dfae84f9325b56b0a9
MarcManiez <marc.maniez@gmail.com>
2017-12-28 21:49:31 -0800

    Define and use validation method for tracks.

--- app/src/models/track.rs
+++ app/src/models/track.rs
@@ -28,15 +28,13 @@ pub struct NewTrack<'a> {
 
 impl Track {
     pub fn new<'a>(length: Option<i32>, link: &'a str, name: Option<&'a str>) -> Result<NewTrack<'a>, String> {
-        let youtube_url_regex = Regex::new("https?:\x2F\x2F(w{3}\x2E)?youtu(be\x2Ecom|\x2Ebe)\x2F.+").expect("Failed to parse regex");
-        if youtube_url_regex.is_match(link) {
-            Ok(NewTrack {
+        match Track::validate(length, link, name) {
+            Some(error) => Err(format!("Error validating track: {}", error)),
+            None => Ok(NewTrack {
                 length,
                 link,
                 name
-            })
-        } else {
-            Err(String::from("Link is not a youtube URL."))
+            }),
         }
     }
 
@@ -45,10 +43,20 @@ impl Track {
             Ok(new_track) =>
                 match new_track.save() {
                     Ok(track) => Ok(track),
-                    Err(error) => Err(format!("Error saving track to database: {:?}", error))
+                    Err(error) => Err(format!("Error saving track to database: {:?}", error)),
                 }
             Err(error) => Err(error)
         }
+    }
+
+    pub fn validate<'a>(_length: Option<i32>, link: &'a str, _name: Option<&'a str>) -> Option<String> {
+        // TODO: to scale this, iterate over a vector of validation closures (one for each rule) and return the first one that
+        // gives us a string (that means something went wrong in the validation process and we should propagate the message)
+        let youtube_url_regex = Regex::new("https?:\x2F\x2F(w{3}\x2E)?youtu(be\x2Ecom|\x2Ebe)\x2F.+").expect("Failed to parse regex");
+        if !youtube_url_regex.is_match(link) {
+            return Some(String::from("Link is not a youtube URL."))
+        }
+        None
     }
 }
 
