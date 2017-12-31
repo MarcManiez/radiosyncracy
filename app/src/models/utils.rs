9105d8a6b9da5eda52174b7ac76d03f3e91eb77d
MarcManiez <marc.maniez@gmail.com>
2017-12-30 19:49:10 -0800

    Add utility to print SQL statements.

--- app/src/models/utils.rs
+++ app/src/models/utils.rs
@@ -1,3 +1,13 @@
+use diesel::query_builder;
+use diesel::pg::Pg;
+
 pub trait Deletable<T=Self> {
     fn delete(&self) -> Result<T, String>;
+}
+
+pub fn print<T>(query: T) -> T
+    where T: query_builder::QueryFragment<Pg>,
+{
+    println!("{}", query_builder::debug_query::<Pg, _>(&query));
+    return query
 }
