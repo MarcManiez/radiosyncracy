62befbd415fa4e6fa200069998983a70b1b17db0
MarcManiez <marc.maniez@gmail.com>
2018-01-01 16:37:32 -0800

    Reset ID incrementation before tests.

---
+++ app/src/tests/mod.rs
@@ -0,0 +1,35 @@
+use diesel::Connection;
+
+use std::env;
+
+use ::connection::{DatabaseConnection, POOL};
+use ::environment::*;
+
+mod models;
+
+fn truncate_all_tables(connection: &DatabaseConnection) {
+    if get() != TEST { return; }
+    connection.execute(
+      "CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$\n\
+      DECLARE\n\
+          statements CURSOR FOR\n\
+              SELECT tablename FROM pg_tables\n\
+              WHERE tableowner = username AND schemaname = 'public' AND tablename != '__diesel_schema_migrations';\n\
+      BEGIN\n\
+          FOR stmt IN statements LOOP\n\
+              EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';\n\
+              EXECUTE 'ALTER SEQUENCE ' || quote_ident(stmt.tablename) || '_id_seq RESTART WITH 1;';\n\
+              EXECUTE 'UPDATE ' || quote_ident(stmt.tablename) || ' SET id = DEFAULT';\n\
+          END LOOP;\n\
+      END;\n\
+      $$ LANGUAGE plpgsql;"
+    ).unwrap();
+    let database_username = env::var("DATABASE_USERNAME").expect("Failed to load database username.");
+    connection.execute(&format!("SELECT truncate_tables('{}');", database_username)).unwrap();
+}
+
+pub fn describe(test: fn(connection: &DatabaseConnection) -> ()) {
+    let database_connection = POOL.get().expect("Failed to fetch a connection.");
+    truncate_all_tables(&database_connection);
+    test(&database_connection);
+}
