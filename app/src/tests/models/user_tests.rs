29bb28ed628a56dc6f09be1e3761e6122d2e7d1a
marcmaniez <marc.maniez@gmail.com>
2018-01-03 22:30:31 -0800

    Define user creation tet.

--- app/src/tests/models/user_tests.rs
+++ app/src/tests/models/user_tests.rs
@@ -1,4 +1,10 @@
+use diesel::prelude::*;
+
+use std::ops::Deref;
+
+use ::connection::POOL;
 use ::models::user::User;
+use ::schema::users;
 use ::tests::truncate_all_tables;
 use ::tests::factories::user::*;
 
@@ -10,3 +16,15 @@ fn find() {
 
     assert_eq!(user.id, found_user.id);
 }
+
+#[test]
+fn create() {
+    truncate_all_tables();
+    let database_connection = POOL.get().expect("Failed to fetch a connection");
+    let user_email = "classeman@topofthe.pop";
+    let _user = User::create("GeorgeAbitbol", user_email, "rosebud");
+    let user = users::table.filter(users::email.eq(user_email)).get_result::<User>(database_connection.deref()).unwrap();
+
+    assert_eq!(user.email, user_email);
+}
+
