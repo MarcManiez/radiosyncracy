4554fc8f5e85b75672bf823a4fe1a5d4679ea905
marcmaniez <marc.maniez@gmail.com>
2018-01-15 11:16:05 -0800

    Add radio factory method for user association.

--- app/src/tests/factories/radio.rs
+++ app/src/tests/factories/radio.rs
@@ -1,5 +1,13 @@
+use ::models::user::User;
 use ::models::radio::Radio;
+use super::user::create_basic_user;
 
 pub fn create_basic_radio() -> Radio {
     Radio::create(None, "Hits from the '90s").unwrap()
+}
+
+pub fn create_radio_with_user() -> (Radio, User) {
+    let user = create_basic_user();
+    let radio = Radio::create(Some(user.id), "Hits from the '90s").unwrap();
+    (radio, user)
 }
